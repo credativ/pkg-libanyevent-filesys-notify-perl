cac6d22883c51aa25ef1c4d1c3fba5ecf36dbc89
Carsten Wolff <carsten@wolffcarsten.de>
2014-07-14 10:26:45 +0200

    Imported Upstream version 0.19

--- lib/AnyEvent/Filesys/Notify/Role/KQueue.pm
+++ lib/AnyEvent/Filesys/Notify/Role/KQueue.pm
@@ -1,4 +1,4 @@
-package AnyEvent::Filesys::Notify::Role::FreeBSD;
+package AnyEvent::Filesys::Notify::Role::KQueue;
 
 # ABSTRACT: Use IO::KQueue to watch for changed files
 
@@ -16,24 +16,27 @@ sub _init {
 
     my @fhs = map {
         open my $fh, '<', $_ or croak "Can't open file: $_";
+
         $kqueue->EV_SET(
             fileno($fh),
-            EVFILT_VNODE | EVFILT_READ,
-            EV_ADD | EV_CLEAR,
-            NOTE_DELETE | NOTE_WRITE | NOTE_RENAME | NOTE_REVOKE
+            EVFILT_VNODE,
+            EV_ADD | EV_ENABLE,
+            NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB |
+            NOTE_LINK | NOTE_RENAME | NOTE_REVOKE,
         );
         $fh;
     } @{ $self->dirs };
 
-    my $w; $w = AE::io $$kqueue, 0, sub {
-        if (my @events = $kqueue->kevent) {
+    my $w;
+    $w = AE::io $$kqueue, 0, sub {
+        if ( my @events = $kqueue->kevent ) {
             $self->_process_events(@events);
         }
     };
 
     $self->_fs_monitor($kqueue);
 
-    $self->_watcher({fhs => \@fhs, w => $w});
+    $self->_watcher( { fhs => \@fhs, w => $w } );
 
     return 1;
 }
@@ -46,11 +49,11 @@ __END__
 
 =head1 NAME
 
-AnyEvent::Filesys::Notify::Role::FreeBSD - Use IO::KQueue to watch for changed files
+AnyEvent::Filesys::Notify::Role::KQueue - Use IO::KQueue to watch for changed files
 
 =head1 VERSION
 
-version 0.15
+version 0.19
 
 =head1 CONTRIBUTORS
 
@@ -63,7 +66,7 @@ Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>.
+This software is copyright (c) 2013 by Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
